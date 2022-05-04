25f358ea4a55d9881a2e3dfeda70f741a30d9ddf
stoerchl <9351883+stoerchl@users.noreply.github.com>
2022-05-04 13:46:58 +0200

    Recursive and output directory option to dump decoded GootLoader script

--- gootloader/decode.py
+++ gootloader/decode.py
@@ -4,9 +4,9 @@
 # description       : Extracts URLs from Gootloader JavaScript
 # author            : @stoerchl
 # email             : patrick.schlapfer@hp.com
-# date              : 20220502
-# version           : 2.5
-# usage             : python decode.py -d <directory_to_search> [-r]
+# date              : 20220504
+# version           : 2.6
+# usage             : decode.py -d <directory_to_search> [-o <output_directory>] [-r]
 # license           : MIT
 # py version        : 3.9.1
 #==============================================================================
@@ -24,6 +24,16 @@ Example:
             Gootloader script must be supplied as an argument.
 
                 $ python decode.py -d samples/20210222/
+
+            Additional arguments can be added to dump the decoded
+            GootLoader script as well as to execute a recursive 
+            search through the provided directory.
+
+            Recursive search:
+                $ python decode.py -d samples/20220405/ -r
+
+            Dump decoded scripts:
+                $ python decode.py -d samples/20220405/ -o decoded_scripts/
 
 As threat actors constantly change their techniques this automation might not
 work on future Gootloader scripts. It should however provide a starting
@@ -65,110 +75,118 @@ def decode_cipher(cipher):
     return plaintext
 
 try:
-    opts, arg = getopt.getopt(all_args, 'd:r')
+    opts, arg = getopt.getopt(all_args, 'd:ro:')
     if len(opts) < 1:
-        print ('usage: decode.py -d <directory_to_search> [-r]')
+        print ('usage: decode.py -d <directory_to_search> [-o <output_directory>] [-r]')
     else:
         folder = None
-        dump_raw = False
+        recursive = False
+        output_folder = None
         for opt, arg_val in opts:
             if opt == "-d":
                 folder = Path(arg_val)
             elif opt == "-r":
-                dump_raw = True
+                recursive = True
+            elif opt == "-o":
+                output_folder = Path(arg_val)
+                output_folder.mkdir(parents=False, exist_ok=True)
 
         if folder == None or not folder.is_dir():
-            print ('usage: decode.py -d <directory_to_search> [-r]')
+            print ('usage: decode.py -d <directory_to_search> [-o <output_directory>] [-r]')
             sys.exit(2)
 
         all_domains = set()
         all_urls = set()
-        all_files = sorted(list(folder.rglob("*")))
+        if recursive:
+            all_files = sorted(list(folder.rglob("*")))
+        else:
+            all_files = sorted(list(folder.glob("*")))
         for f in all_files:
             try:
-                js = open(f)
-                content = js.read()
+                if f.is_file():
+                    js = open(f)
+                    content = js.read()
                 
-                if len(content) > 100000: # new version. file contains library code to obfuscate
-                    clean_content = ""
-                    matches = re.findall(functions_regex, content, re.MULTILINE)
-                    for m in matches:
-                        clean_content += m + "\n"
-
-                    matches = re.findall(ext_code_regex, clean_content, re.MULTILINE)
-                    code_parts = dict()
-                    for m in matches:
-                        code_parts[m[0]] = m[1]
-                    
-                    matches = re.findall(code_order.replace("NUM_REP", str(len(code_parts)-1)), clean_content.replace(" ", ""), re.MULTILINE)
-                    order = list()
-                    if len(matches) > 0:
+                    if len(content) > 100000: # new version. file contains library code to obfuscate
+                        clean_content = ""
+                        matches = re.findall(functions_regex, content, re.MULTILINE)
                         for m in matches:
-                            order = m.split("+")
-                    else:
-                        # New GootLoader Version 2022-05
-                        code_fragments = dict()
-                        result_element = ""
-                        matches = re.findall(re_code_order, clean_content.replace(" ", ""), re.MULTILINE)
-                        for expr in matches:
-                            stmt = expr.replace(" ", "").split("=")
-                            code_fragments[stmt[0]] = stmt[1].split("+")
-                            result_element = code_fragments[stmt[0]]
-                        
-                        for element in result_element:
-                            order += code_fragments[element]
+                            clean_content += m + "\n"
 
-                        
-                    ordered_code = ""
-                    for element in order:
-                        ordered_code += code_parts[element]
-                    content = "'" + ordered_code + "'"
+                        matches = re.findall(ext_code_regex, clean_content, re.MULTILINE)
+                        code_parts = dict()
+                        for m in matches:
+                            code_parts[m[0]] = m[1]
                     
-                round = 0
-                while round < 2:
-                    matches = re.findall(code_regex, content, re.MULTILINE)
-                    longest_match = ""
-                    for m in matches:
-                        if len(longest_match) < len(m):
-                            longest_match = m
+                        matches = re.findall(code_order.replace("NUM_REP", str(len(code_parts)-1)), clean_content.replace(" ", ""), re.MULTILINE)
+                        order = list()
+                        if len(matches) > 0:
+                            for m in matches:
+                                order = m.split("+")
+                        else:
+                            # New GootLoader Version 2022-05
+                            code_fragments = dict()
+                            result_element = ""
+                            matches = re.findall(re_code_order, clean_content.replace(" ", ""), re.MULTILINE)
+                            for expr in matches:
+                                stmt = expr.replace(" ", "").split("=")
+                                code_fragments[stmt[0]] = stmt[1].split("+")
+                                result_element = code_fragments[stmt[0]]
+                        
+                            for element in result_element:
+                                order += code_fragments[element]
+
+                        
+                        ordered_code = ""
+                        for element in order:
+                            ordered_code += code_parts[element]
+                        content = "'" + ordered_code + "'"
                     
-                    content = decode_cipher(decode(encode(longest_match, 'latin-1', 'backslashreplace'), 'unicode-escape')) #
-                    round += 1
+                    round = 0
+                    while round < 2:
+                        matches = re.findall(code_regex, content, re.MULTILINE)
+                        longest_match = ""
+                        for m in matches:
+                            if len(longest_match) < len(m):
+                                longest_match = m
+                    
+                        content = decode_cipher(decode(encode(longest_match, 'latin-1', 'backslashreplace'), 'unicode-escape')) #
+                        round += 1
 
-                if dump_raw:
-                    decoded_path = f.parent / ("decoded_" + f.name)
-                    w = open(decoded_path,"w")
-                    w.write(content)
-                    w.close()
-                    print("Wrote " + str(decoded_path))
+                    if output_folder:
+                        decoded_path = output_folder / ("decoded_" + f.name)
+                        w = open(decoded_path,"w")
+                        w.write(content)
+                        w.close()
+                        print("Wrote " + str(decoded_path))
 
-                domains = re.findall(breacket_regex, content.split(";")[0], re.MULTILINE)
-                urls = re.findall(url_regex, content, re.MULTILINE)
-                if len(urls) > 0:
-                    replaceables = re.findall(separator_regex, urls[0], re.MULTILINE)
-                    if len(replaceables) == 2:
-                        for d in domains:
-                            for dom in d.replace("\"", "").replace("'", "").split(","):
-                                all_domains.add(dom)
-                                all_urls.add(urls[0].replace(replaceables[0], dom).replace(replaceables[1], "") + "=")
-                                
-                    print("OK - " + str(f))
-                else:
-                    # New GootLoader Version 2022-04
-                    content = content.replace("\")+(\"", "").replace("\")+\"", "").replace("\"+(\"", "").replace("\")+", "").replace("+(\"", "")
                     domains = re.findall(breacket_regex, content.split(";")[0], re.MULTILINE)
                     urls = re.findall(url_regex, content, re.MULTILINE)
                     if len(urls) > 0:
-                        replaceables = re.findall(array_replace_regex, urls[0], re.MULTILINE)
-                        if len(replaceables) > 0:
+                        replaceables = re.findall(separator_regex, urls[0], re.MULTILINE)
+                        if len(replaceables) == 2:
                             for d in domains:
                                 for dom in d.replace("\"", "").replace("'", "").split(","):
                                     all_domains.add(dom)
-                                    all_urls.add(urls[0].replace(replaceables[0], dom) + "=")
-
+                                    all_urls.add(urls[0].replace(replaceables[0], dom).replace(replaceables[1], "") + "=")
+                                
                         print("OK - " + str(f))
                     else:
-                        print("NOK - " + str(f))
+                        # New GootLoader Version 2022-04
+                        content = content.replace("\")+(\"", "").replace("\")+\"", "").replace("\"+(\"", "").replace("\")+", "").replace("+(\"", "")
+                        domains = re.findall(breacket_regex, content.split(";")[0], re.MULTILINE)
+                        urls = re.findall(url_regex, content, re.MULTILINE)
+                        if len(urls) > 0:
+                            replaceables = re.findall(array_replace_regex, urls[0], re.MULTILINE)
+                            if len(replaceables) > 0:
+                                for d in domains:
+                                    for dom in d.replace("\"", "").replace("'", "").split(","):
+                                        all_domains.add(dom)
+                                        all_urls.add(urls[0].replace(replaceables[0], dom) + "=")
+
+                            print("OK - " + str(f))
+                        else:
+                            print("NOK - " + str(f))
                     
             except Exception as e:
                 print(e)
@@ -189,7 +207,7 @@ try:
         print("> Wrote file: domains.txt")
 
 except getopt.GetoptError:
-    print ('usage: decode.py -d <directory_to_search> [-r]')
+    print ('usage: decode.py -d <directory_to_search> [-o <output_directory>] [-r]')
     sys.exit(2)
 except Exception as e:
     print(e)
